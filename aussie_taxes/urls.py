ad4031adee4066e2d05f34f770f21de56547a3cd
Roger Sherman <rsherman@viddler.com>
2016-03-04 00:41:11 -0500

    Contact and About pages set up

--- aussie_taxes/urls.py
+++ aussie_taxes/urls.py
@@ -13,13 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, patterns, include
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from aussie_monthly_net import views
 
-
 urlpatterns = [
     url(r'^$', views.index, name='home'),
+    url(r'^about/$',
+        TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^contact/$',
+        TemplateView.as_view(template_name='contact.html'), name='contact'),
     url(r'^admin/', include(admin.site.urls))
 ]
